baee3f059367634d4af5433b6279418c7dcac4e6
will-o-the-wisp <kli18@stuy.edu>
2018-11-06 13:15:13 -0500

    created columnSum

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -19,8 +19,8 @@ public class ArrayMethods{
    public static int columnSum(int[][] ary, int x){
      int ans = 0;
      for(int i=0;i<ary.length;i++){
-       if(x>=ary[i].length){
-         
+       if(x<ary[i].length){
+         ans+=ary[i][x];
        }
      }
      return ans;
@@ -31,11 +31,11 @@ public class ArrayMethods{
    /*
    *PART 2 - use prior methods where appropriate
    */
-   public static int[] allRowSums(int[][] ary){}
+  // public static int[] allRowSums(int[][] ary){}
     //returns an array of the row sums for each row of ary.
     //Index i of the return array contains the sum of elements in row i.
 
-    public static int[] allColSums(int[][] ary){}
+//    public static int[] allColSums(int[][] ary){}
     //Returns an array with the column sum of each column of ary.
     //When a row is not long enough to reach the column count it as a zero. (NO indexOutOfBounds should ever occur)
     //Index i of the return array contains the sum of elements in column i, ignoring any rows that are too short.
@@ -45,10 +45,10 @@ public class ArrayMethods{
    /*
    *PART 3 - use prior methods where appropriate
    */
-   public static boolean isRowMagic(int[][] ary){}
+  // public static boolean isRowMagic(int[][] ary){}
      //checks if the array is row-magic (this means that every row has the same row sum).
 
-   public static boolean isColumnMagic(int[][] ary){}
+//   public static boolean isColumnMagic(int[][] ary){}
     //checks if the array is column-magic (this means that every column has the same column sum).
 
 }
